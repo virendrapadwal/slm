6f6ae5569e6920e160ec2ad04cbf53dc71162620
virendra <virendra.padwal@siemens.com>
2024-02-19 13:13:19 +0530

    write

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express();
-const fs = require('fs');
-
-
-
-
+const fs = require('fs-extra');
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
 app.get("/write",(req,res) => {
     fs.writeFileSync("/virendra/test.txt","Hello virendra");
     res.send("DOne");
+})
+
+app.post("/maximo",(req,res) => {
+
+    fs.writeFileSync("/virendra/"+Date.now() + ".txt",req.body);
+    res.send("Done");
 })
 app.use(express.static("/virendra"));
 app.get("*",(req,res)=> { res.send("Hello bsdk")})
