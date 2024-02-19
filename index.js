2c91e022d69ce6c1cf7b4eede9c07e64b1b262b9
virendra <virendra.padwal@siemens.com>
2024-02-19 13:22:44 +0530

    write path 3

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const fs = require('fs-extra');
-const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 4000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(bodyParser.json())
-
-app.get("/write",(req,res) => {
-    fs.writeFileSync("/virendra/test.txt","Hello virendra");
-    res.send("DOne");
-})
-
-app.post("/maximo",(req,res) => {
-
-    fs.writeFileSync("/virendra/new.txt",req.body);
+app.get("/write", (req, res) => {
+    fs.writeFileSync("/virendra/test.txt", "Hello virendra");
     res.send("Done");
-})
+});
+
+app.post("/maximo", (req, res) => {
+    fs.writeFileSync("/virendra/new.txt", JSON.stringify(req.body));
+    res.send("Done");
+});
+
 app.use(express.static("/virendra"));
-app.get("/up",(req,res)=> { res.send("Hello bsdk")})
+app.get("/up", (req, res) => { res.send("Hello"); });
 
-
-app.listen(PORT,()=> { console.info(`server is listening on ${PORT}`)})
+app.listen(PORT, () => { console.info(`server is listening on ${PORT}`); });
