44685594ca0211fe7aa2082e6cca0e182fb9a903
virendra <virendra.padwal@siemens.com>
2024-02-19 12:06:19 +0530

    test file writing

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express();
+const fs = require('fs');
+
+
+
+
 const PORT = process.env.PORT || 4000;
+app.get("/write",(req,res) => {
+    fs.writeFileSync("/virendra/test.txt","Hello virendra");
+})
+app.use(express.static("/virendra"));
 app.get("*",(req,res)=> { res.send("Hello bsdk")})
+
 
 app.listen(PORT,()=> { console.info(`server is listening on ${PORT}`)})
